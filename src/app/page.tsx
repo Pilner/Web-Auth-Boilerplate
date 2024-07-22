bc0eadfe470ab6fe0df7cef18fea1fdbef75ab5f
Pilner <raileyvictuelles@gmail.com>
2024-07-23 03:33:35 +0800

    feat: initial home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import { Navbar } from "@/_components/semantics/Navbar"
+import { Footer } from "@/_components/semantics/Footer"
+
 export default function Home() {
   return (
-	<div>Home</div>
+	<>
+		<Navbar />
+		<div className="container">
+			<section style={{height:"70vh"}}>
+				
+			</section>
+		</div>
+		<Footer />
+	</>
   )
 }
