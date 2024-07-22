eba059a7c94d2ec8114d6f1b85e1191a318fc11c
Pilner <raileyvictuelles@gmail.com>
2024-07-23 02:23:10 +0800

    feat: added navbar

---
+++ src/_components/semantics/Navbar.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { LinkButton } from "@/_components/Button";
+import styles from "./styles/Navbar.module.css";
+
+export function Navbar() {
+	return (
+		<nav id={styles.navbar}>
+			<div className="container">
+				<div id={styles.navLogoSide}>
+					<Link href="/">
+						<div id={styles.navLogoImg}>
+								<Image
+									src="/images/placeholder.png"
+									alt="Logo"
+									width={0}
+									height={0}
+									style={{
+										height: "100%",
+										width: "auto",
+										objectFit: "contain",
+									}}
+									unoptimized={true}
+								/>
+						</div>
+						<div id={styles.navLogotext}>
+							<p>Logo</p>
+						</div>
+					</Link>
+				</div>
+				<div id={styles.navLinkSide}>
+					<div>
+						<ul>
+							<li>
+								<Link href="/" scroll={false}>
+									Home
+								</Link>
+							</li>
+							<li>
+								<Link href="/" scroll={false}>
+									About
+								</Link>
+							</li>
+							<li>
+								<Link href="/" scroll={false}>
+									Contact
+								</Link>
+							</li>
+							<li>
+								<LinkButton text="Login" href="/login"></LinkButton>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+}
