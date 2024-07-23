f16ddd9866d223189624a2b6d77b05e542841ab8
Pilner <raileyvictuelles@gmail.com>
2024-07-23 14:49:32 +0800

    refactor: moved button component into ui folder - Under `_components/` will have a new folder: `ui/` - This will include Buttons, Inputs, etc.

--- src/_components/semantics/Navbar.tsx
+++ src/_components/semantics/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { LinkButton } from "@/_components/Button";
+import { LinkButton } from "@/_components/ui/Button";
 import styles from "./styles/Navbar.module.css";
 
 export function Navbar() {
