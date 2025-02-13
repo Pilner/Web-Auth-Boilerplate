350e48d3f1903b109782071de5c258366df4123d
Pilner <raileyvictuelles@gmail.com>
2025-02-13 12:27:19 +0800

    feat: replaced tailwind cdn with npm package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import { Providers } from '@/app/providers';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'Web Auth Boilerplate',
-	description: 'A Web Auth Boilerplate that uses Next.js and Next-Auth',
+	description: 'A Web Auth Boilerplate that uses Next.js, TailwindCSS, and Next-Auth',
 };
 
 export default function RootLayout({
@@ -17,10 +18,8 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<head>
-				<script src="https://kit.fontawesome.com/ad98de5722.js" crossOrigin="anonymous"></script>
-			</head>
-			<body className={inter.className}>
+			<head></head>
+			<body className={`bg-gray ${inter.className}`}>
 				<Providers>{children}</Providers>
 			</body>
 		</html>
