8f3ad0c78996094d4797957de124b18261f60fd8
Pilner <raileyvictuelles@gmail.com>
2025-02-14 18:56:49 +0800

    chore: clean

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -34,39 +34,3 @@ export default function NotFound() {
 		</main>
 	);
 }
-
-{
-	/* <>
-			<Navbar />
-			<section id={styles.notFoundPage}>
-				<div className="container">
-					<div id={styles.errorMessage}>
-						<div id={styles.errorPicture}>
-							<Image
-								src="/images/questionmark.png"
-								alt="Not Found Image"
-								width={0}
-								height={0}
-								style={{
-									width: 'auto',
-									height: '100%',
-								}}
-								unoptimized={true}
-							/>
-						</div>
-						<h1 className={styles.errorTitleFont}>
-							404 NOT
-							<br />
-							<span className="main-accent">FOUND</span>
-						</h1>
-						<p className={styles.errorSubtitleFont}>
-							Oops! The page you&apos;re looking for
-							<br />
-							does not exist.
-						</p>
-					</div>
-				</div>
-			</section>
-			<Footer />
-		</> */
-}
